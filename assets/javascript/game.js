ac5ac56c921f82da869114f0665ac1c0589218fc
Ali <ali@Alis-MBP.tremontplacelofts.local>
2018-02-13 13:27:45 -0500

    please work 127pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 //array of words
 const wordList = ["buzz", "fizzy", "guevara", "pipboy", "bobmarley", "pirate"];
 
+var myKitten = "jaspercat";
 //alphabet array
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
